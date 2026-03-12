9fd40946afb608171ce4e8a0be04a181c9e1d050
joulong <167416274+joulong@users.noreply.github.com>
2026-03-12 18:41:08 +0800

    Add async function to load navigation from HTML
    Signed-off-by: joulong <167416274+joulong@users.noreply.github.com>

--- js/loadNav.js
+++ js/loadNav.js
@@ -1,1 +1,11 @@
+async function loadNav(){
 
+const res = await fetch("components/nav.html")
+
+const html = await res.text()
+
+document.getElementById("nav-container").innerHTML = html
+
+}
+
+window.addEventListener("DOMContentLoaded", loadNav)
